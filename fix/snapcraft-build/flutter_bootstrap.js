6e41e9401b22b9db27ab6ad6e4eae0433d5faa5f
getBoolean <19920697+getBoolean@users.noreply.github.com>
2026-02-11 03:14:36 +0000

    Deploying to gh-pages from @ getBoolean/remote_zfs_unlock@07bf7dd49ee16592d83fd19953820e1ca803e164 🚀

--- fix/snapcraft-build/flutter_bootstrap.js
+++ fix/snapcraft-build/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"492bf6df86d056e47d05a3d5404f3d78939b3d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3018312645" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4233106744" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
