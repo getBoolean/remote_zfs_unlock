ce6bc2dcdc3589584042f08fdd54e51fbb42702f
getBoolean <19920697+getBoolean@users.noreply.github.com>
2026-02-13 08:45:07 +0000

    Deploying to gh-pages from @ getBoolean/remote_zfs_unlock@cba23b0fdf608824042d26c37f493faa858894cd 🚀

--- refactor/riverpod/flutter_bootstrap.js
+++ refactor/riverpod/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"492bf6df86d056e47d05a3d5404f3d78939b3d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2936727662" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3799446522" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
